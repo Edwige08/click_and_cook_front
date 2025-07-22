a673200f75c6547e9445c823ff2ab91b0527982d
Edwige08 <edwige.saves@gmail.com>
2025-07-22 11:06:03 +0200

    create: CardPresentation for the home page

---
+++ src/app/components/CardPresentation.tsx
@@ -0,0 +1,26 @@
+export default function CardPresentation(props: { emoji: string, title: string, description: string }) {
+    return (
+        <article
+            className="m-5 px-5 py-10 border rounded-xl shadow-sm bg-(--lightColor)"
+        >
+            <div
+                className="flex justify-center items-center"
+            >
+                <p
+                    className="p-5 rounded-xl text-3xl bg-(--orangeColor)"
+                >
+                    {props.emoji}
+                </p>
+            </div>
+            <h3
+            className="p-3 font-bold text-xl text-center"
+            >
+                {props.title}
+            </h3>
+            <p
+            className="text-center">
+                {props.description}
+            </p>
+        </article>
+    )
+}
