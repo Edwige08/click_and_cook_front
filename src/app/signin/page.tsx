4328816ed4c8953d59df644f55310912c8a66135
Edwige08 <edwige.saves@gmail.com>
2025-07-22 14:26:30 +0200

    update: sign in page adding form

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
 import Navbar from "../components/Navbar";
+import FormSignIn from "../components/FormSignIn";
+import Link from "next/link";
 
-export default function Home () {
+export default function Home() {
     return (
         <>
-        <Navbar />
+            <header
+                className="flex flex-col items-center bg-(--lightColor)"
+            >
+                <Link href="/">
+                    <Image
+                        src="/images/logo_click_and_cook.png"
+                        alt="Logo Click & Cook"
+                        width={1024}
+                        height={1024}
+                        className="w-50 m-3"
+                    />
+                </Link>
+                <div>
+                    <FormSignIn />
+                </div>
+            </header>
         </>
     )
 }
