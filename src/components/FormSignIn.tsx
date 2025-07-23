ecfeea0ecdf64ecdd46e9a432f12c87138b21509
Edwige08 <edwige.saves@gmail.com>
2025-07-23 16:37:33 +0200

    add: link to the back on FormSignIn.tsx

--- src/components/FormSignIn.tsx
+++ src/components/FormSignIn.tsx
@@ -3,22 +3,55 @@
 import Link from "next/link";
 import ButtonOrange from "./ButtonOrange";
 import Input from "./Input";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function FormSignIn() {
+    const router = useRouter()
 
     const [formData, setFormData] = useState({
         username: '',
         password: ''
     });
+    const [message, setMessage] = useState<string>();
 
     const HandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setFormData((prev) => ({ ...prev, [event.target.name]: event.target.value }))
     }
 
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_END_URL}/login/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Erreur lors de la connexion')
+            }
+
+            setMessage(data.message)
+            console.log(data)
+
+            setTimeout(() => {
+                router.push('/home')
+            }, 2000);
+        } catch (err) {
+            const errorMessage = err instanceof Error ? err.message : 'Une erreur est survenue';
+            setMessage(errorMessage);
+        }
+    }
+
     return (
         <form
             className="flex flex-col gap-4 items-center px-10 py-5 m-5 rounded-sm shadow-lg bg-white"
+            onSubmit={handleSubmit}
         >
             <h2
                 className="font-bold text-xl mb-2"
