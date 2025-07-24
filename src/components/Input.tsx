bfbee65365b9b09882cb3234a80442752422175a
Edwige08 <edwige.saves@gmail.com>
2025-07-24 15:53:27 +0200

    update: component Input.tsx adding the possibility to add more classes when called

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,12 @@
 import { ChangeEventHandler } from "react";
 
-export default function Input(props: { inputName: string, type: string, placeholder?: string, value?: string, onChange?: ChangeEventHandler }) {
+export default function Input(props: { inputName: string, type: string, placeholder?: string, classes?: string, value?: string, onChange?: ChangeEventHandler }) {
     return (
         <input
             name={props.inputName}
             type={props.type}
             placeholder={props.placeholder}
-            className="p-2 rounded-sm border bg-(--lightColor)"
+            className={`p-2 rounded-sm border bg-(--lightColor) ${props.classes}`}
             value={props.value}
             onChange={props.onChange}
             required
