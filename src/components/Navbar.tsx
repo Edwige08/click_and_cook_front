a68f40ab8e4e093700c306caeb75916b70dca464
Edwige08 <edwige.saves@gmail.com>
2025-07-25 10:32:14 +0200

    update: navbar adding links and a button to disconnect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,63 @@
 import Image from "next/image";
-import { Home, UserRoundPen, ConciergeBell } from "lucide-react"
+import { Home, UserRoundPen, ConciergeBell, LogOut } from "lucide-react"
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <nav
             className="flex flex-row justify-between items-center py-2 px-7 bg-(--lightColor)"
         >
-            <Image
-                src="/images/logo_click_and_cook.png"
-                alt="Logo Click & Cook"
-                width={1024}
-                height={1024}
-                className="w-30"
-            />
+            <Link
+                href="/home">
+                <Image
+                    src="/images/logo_click_and_cook.png"
+                    alt="Logo Click & Cook"
+                    width={1024}
+                    height={1024}
+                    className="w-30"
+                />
+            </Link>
             <div>
-                <ul className="flex flex-col justify-center gap-2 h-full md:flex-row md:gap-8">
+                <ul className="flex flex-col justify-center gap-1 h-full md:flex-row md:gap-8">
                     <li>
-                        <a href="/home" className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
+                        <Link
+                            href="/home"
+                            className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
                             <Home />
                             <p className="flex flex-col justify-end">
                                 Accueil
                             </p>
-                        </a>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
+                        <Link
+                            href="/home/personnalProfile"
+                            className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
                             <UserRoundPen />
                             <p className="flex flex-col justify-end">
                                 Mon profil
                             </p>
-                        </a>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
+                        <Link
+                            href="#"
+                            className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
                             <ConciergeBell />
                             <p className="flex flex-col justify-end">
                                 Mes recettes
                             </p>
-                        </a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="#"
+                            className="flex flex-row gap-2 items-center md:w-30 hover:font-bold md:border-b md:py-2 md:flex-col md:rounded-t-4xl md:hover:bg-(--orangeColor)">
+                            <LogOut />
+                            <p className="flex flex-col justify-end">
+                                Déconnexion
+                            </p>
+                        </Link>
                     </li>
                 </ul>
             </div>
