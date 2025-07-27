3c1a3a89a6899d3765bcd1317e7aff60f9f30d1b
Edwige08 <edwige.saves@gmail.com>
2025-07-27 17:32:55 +0200

    wip: fetch recipes on page home and modify FormSearchBar with props

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardRecipe from "../../components/CardRecipe";
 import Navbar from "../../components/Navbar";
 import FormSearchBar from "@/components/FormSearchBar";
@@ -21,27 +21,88 @@ const recipe: Recipe = {
 
 export default function Home() {
 
+    const [searchValue, setSearchValue] = useState<string>("");
     const [lastRecipes, setLastRecipes] = useState([]);
 
-    async function getRecipes () {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_END_URL}`)
-        const data = await response.json();
-        setLastRecipes(data)
+    const username: string | null = localStorage.getItem("username");
+    console.log(username);
+
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        try {
+            // async function getSearchRecipes() {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_END_URL}/api/recipes`, {
+                // credentials: 'include',
+                headers: {
+                    'Authorization': 'Token c29fccbebea1072ceebb4eb212c3da1608d0fb56',
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+
+            const data = await response.json();
+            console.log("data :", data)
+            // }
+        } catch (err) {
+            const errorMessage = err instanceof Error ? err.message : "Une erreur est survenue dans la récupération des données.";
+            console.log("errorMessage : ", errorMessage);
+        }
+    }
+
+    useEffect(() => {
+        async function getRecipes() {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_END_URL}/api/recipes`, {
+                // credentials: 'include',
+                headers: {
+                    'Authorization': 'Token c29fccbebea1072ceebb4eb212c3da1608d0fb56',
+                    'Content-Type': 'application/json',
+                }
+            })
+            const data = await response.json();
+            console.log("data :", data)
+            setLastRecipes(data)
+        }
+        getRecipes();
+    }, [])
+
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value)
     }
 
     return (
         <>
             <h2 className="py-5 text-center text-xl font-bold">
-                Bonjour USER !
+                Bonjour {username} !
             </h2>
 
-            <FormSearchBar />
+            <FormSearchBar
+                onSubmit={handleSubmit}
+                value={searchValue}
+                onChange={handleChange}
+            />
 
             <p className="p-5 text-center">
                 Voici les dernières recettes tout juste sorties du four :
             </p>
 
             <div>
+                {/* {lastRecipes.map((recipe, index) => {
+                    return (
+                        <CardRecipe
+                            key={index}
+                            title={recipe.titre}
+                            photo={recipe.photo}
+                            date={recipe.date}
+                            auteurice={recipe.auteurice}
+                        />
+                    )
+                })} */}
                 <CardRecipe
                     title={recipe.titre}
                     photo={recipe.photo}
--- src/components/FormSearchBar.tsx
+++ src/components/FormSearchBar.tsx
@@ -1,11 +1,12 @@
 import ButtonSearch from "./ButtonGreen";
 import Input from "./Input";
 
-export default function FormSearchBar() {
+export default function FormSearchBar(props: {onSubmit: React.FormEventHandler, value: string, onChange: React.ChangeEventHandler}) {
     return (
         <form
             action=""
             className="flex flex-row gap-2 justify-center m-2 p-3 rounded-lg shadow-xl bg-(--orangeColor)"
+            onSubmit={props.onSubmit}
         >
             <div
                 className="flex flex-col gap-2 w-[90%]">
@@ -18,6 +19,8 @@ export default function FormSearchBar() {
                     inputName="search"
                     type="text" 
                     placeholder="ex : Tarte aux poireaux"
+                    value={props.value}
+                    onChange={props.onChange}
                     />
             </div>
             <div className="flex flex-col justify-end items-end">
