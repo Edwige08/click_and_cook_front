45a014a4c664682ddb6dc5399e5fced48d147f96
rchouhani <rchouhani@me.com>
2025-07-30 09:59:47 +0200

    add: link to recipe by id

--- src/components/MyRecipes.tsx
+++ src/components/MyRecipes.tsx
@@ -1,14 +1,14 @@
 import { ChefHat } from "lucide-react";
 
-export default function MyRecipes(props: { title: string }) {
-    return (
-        <button
-            className="flex flex-row gap-2 items-center border rounded-xl p-2 hover:underline hover:cursor-pointer hover:bg-(--lightColor)"
-        >
-            <p>
-                <ChefHat />
-            </p>
-            <p>{props.title}</p>
-        </button>
-    )
+export default function MyRecipes(props: { title: string, id: number }) {
+  return (
+    <a href={`recipe/${props.id}`} key={props.id}>
+      <button className="flex flex-row gap-2 items-center border rounded-xl p-2 hover:underline hover:cursor-pointer hover:bg-(--lightColor)">
+        <p>
+          <ChefHat />
+        </p>
+        <p>{props.title}</p>
+      </button>
+    </a>
+  );
 }
