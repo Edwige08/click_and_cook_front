76f56dfa18e7c814842521749b6a50b6181f1188
Edwige08 <edwige.saves@gmail.com>
2025-07-30 19:54:37 +0200

    refactoring: delete an import that is never used in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 
 export const metadata: Metadata = {
   title: "Cook & Click",
   description: "Blog de recettes",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="fr">
-      <body
-        className="bg-(--lightColor)"
-      >
+      <body className="bg-(--lightColor)">
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
