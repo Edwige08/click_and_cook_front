08a40de4b5e7ab01d4d575614e43d820a7e360df
Edwige08 <edwige.saves@gmail.com>
2025-07-30 18:22:39 +0200

    fix: css for buttons in MyRecipes.tsx

--- src/components/MyRecipes.tsx
+++ src/components/MyRecipes.tsx
@@ -2,8 +2,8 @@ import { ChefHat } from "lucide-react";
 
 export default function MyRecipes(props: { title: string, id: number }) {
   return (
-    <a href={`recipe/${props.id}`} key={props.id}>
-      <button className="flex flex-row gap-2 items-center border rounded-xl p-2 hover:underline hover:cursor-pointer hover:bg-(--lightColor)">
+    <a href={`recipe/${props.id}`}>
+      <button className="flex flex-row gap-2 items-center border rounded-xl p-2 w-full hover:underline hover:cursor-pointer hover:bg-(--orangeColor)">
         <p>
           <ChefHat />
         </p>
