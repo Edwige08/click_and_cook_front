5856d390d6df35034fa101b579258ae8229d03f9
rchouhani <rchouhani@me.com>
2025-07-30 15:46:04 +0200

    refacto: indentation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,22 @@
 import { ChangeEventHandler } from "react";
 
-export default function Input(props: { inputName: string, type: string, placeholder?: string, classes?: string, value?: string | number, onChange?: ChangeEventHandler }) {
-    return (
-        <input
-            name={props.inputName}
-            type={props.type}
-            placeholder={props.placeholder}
-            className={`p-2 rounded-sm border bg-(--lightColor) ${props.classes}`}
-            value={props.value}
-            onChange={props.onChange}
-            required
-        />
-    )
+export default function Input(props: {
+  inputName: string;
+  type: string;
+  placeholder?: string;
+  classes?: string;
+  value?: string | number;
+  onChange?: ChangeEventHandler;
+}) {
+  return (
+    <input
+      name={props.inputName}
+      type={props.type}
+      placeholder={props.placeholder}
+      className={`p-2 rounded-sm border bg-(--lightColor) ${props.classes}`}
+      value={props.value}
+      onChange={props.onChange}
+      required
+    />
+  );
 }
